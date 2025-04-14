a045480e237d154605a09df6a5f36d1068182e35
Mikhail <mihailsvirukov@mail.ru>
2025-04-14 11:57:01 +0300

    add edge deletion testcase

--- app/src/test/kotlin/model/GraphTest.kt
+++ app/src/test/kotlin/model/GraphTest.kt
@@ -1,6 +1,5 @@
 package model
 
-import androidx.compose.ui.unit.sp
 import model.graphs.AbstractGraph
 import model.graphs.DirWeightGraph
 import model.graphs.DirectedGraph
@@ -9,10 +8,7 @@ import model.graphs.UndirectedGraph
 import org.junit.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.ArgumentsSource
-import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.ValueSource
 import java.util.stream.Stream
 import kotlin.math.min
 import kotlin.random.Random
@@ -23,33 +19,7 @@ import kotlin.test.assertEquals
 
 class GraphTest {
 
-    companion object {
-        val constructors=arrayOf(DirectedGraph::class, DirWeightGraph::class, UndirWeightGraph::class, UndirectedGraph::class)
-        @JvmStatic fun generateVertices(): Stream<Arguments> {
-            return Stream.generate {
-                val numb=Random.nextInt(1,1000)
-                val array= HashMap< Int, Vertex<Int, Int>?>(numb)
-                for (i in 0..<numb)
-                    array[i]= Vertex(Random.nextInt(), Random.nextInt())
-                Arguments.of(numb, array,constructors.random())
-            }.limit(1000)
-        }
-
-    }
-
-    @ParameterizedTest(name = "{0} vertices for {2}")
-    @MethodSource("generateVertices")
-    fun `test vertex insertion`(amount: Int, map: HashMap<Int, Vertex<Int, Int>>, kClass: KClass<AbstractGraph<Int, Int>>) {
-        var graph= kClass.primaryConstructor?.call()
-        for (i in map.values)
-            graph?.addVertex(i)
-        assertEquals(amount, graph?.vertices?.size)
-    }
-
-    @ParameterizedTest(name = "{0} vertices to make edges for {2}")
-    @MethodSource("generateVertices")
-    fun `test edge insertion`(amount: Int, map: HashMap<Int, Vertex<Int, Int>>, kClass: KClass<AbstractGraph<Int, Int>>) {
-        var graph= kClass.primaryConstructor?.call()
+    fun edgeAddition(amount: Int, map: HashMap<Int, Vertex<Int, Int>>, graph: AbstractGraph<Int, Int>): IntArray {
         var array= IntArray(map.keys.size)
         var t=min(2000, amount*amount/2)
         for (i in 1..t) {
@@ -57,14 +27,43 @@ class GraphTest {
             var second=map.keys.random()
             if (first==second)
                 continue
-            graph?.addEdge(map[first] ?: continue, map[second] ?: continue, Random.nextInt())
-            if (kClass.simpleName in arrayOf("UndirectedGraph", "UndirWeightGraph"))
+            graph.addEdge(map[first] ?: continue, map[second] ?: continue, Random.nextInt())
+            if (graph::class.simpleName in arrayOf("UndirectedGraph", "UndirWeightGraph"))
                 array[second]++
             array[first]++
         }
+        return array
+    }
 
+    companion object {
+        val constructors=arrayOf(DirectedGraph::class, DirWeightGraph::class, UndirWeightGraph::class, UndirectedGraph::class)
+        @JvmStatic fun generateVertices(): Stream<Arguments> {
+            return Stream.generate {
+                val numb=Random.nextInt(1,1000)
+                val map= HashMap< Int, Vertex<Int, Int>?>(numb)
+                for (i in 0..<numb)
+                    map[i]= Vertex(Random.nextInt(), Random.nextInt())
+                Arguments.of(numb, map,constructors.random())
+            }.limit(250)
+        }
+    }
+
+    @ParameterizedTest(name = "{0} vertices for {2}")
+    @MethodSource("generateVertices")
+    fun `test vertex insertion`(amount: Int, map: HashMap<Int, Vertex<Int, Int>>, kClass: KClass<AbstractGraph<Int, Int>>) {
+        val graph= kClass.primaryConstructor?.call() ?: return
+        for (i in map.values)
+            graph.addVertex(i)
+        assertEquals(amount, graph.vertices.size)
+    }
+
+    @ParameterizedTest(name = "{0} vertices to make edges for {2}")
+    @MethodSource("generateVertices")
+    fun `test edge insertion`(amount: Int, map: HashMap<Int, Vertex<Int, Int>>, kClass: KClass<AbstractGraph<Int, Int>>) {
+        var graph= kClass.primaryConstructor?.call() ?: return
+        var array=edgeAddition(amount, map, graph)
         for (i in 0..<array.size)
-            assertEquals(array[i], graph?.edges?.get(map[i])?.size ?: 0)
+            assertEquals(array[i], graph.edges[map[i]]?.size ?: 0)
     }
 
     @ParameterizedTest(name = "{0} vertices to delete some from {2}")
@@ -81,6 +80,33 @@ class GraphTest {
             map.remove(cur)
         }
         assertEquals(amount-expected, graph?.vertices?.size ?: 0)
+    }
+
+
+    @ParameterizedTest(name = "{0} vertices to delete edges from {2}")
+    @MethodSource("generateVertices")
+    fun `test edge deletion`(amount: Int, map: HashMap<Int, Vertex<Int, Int>>, kClass: KClass<AbstractGraph<Int, Int>>) {
+        var graph=kClass.primaryConstructor?.call() ?: return
+        var array=edgeAddition(amount, map, graph)
+        var repetitions=Random.nextInt(-1, array.sum())
+        for (i in 1..repetitions) {
+            if (graph.edges.keys.isEmpty())
+                break
+            var vector=graph.edges[map[array.indices.random()]]
+            var current: Edge<Int, Int>?=null
+            if (vector?.isNotEmpty() == true)
+                current=vector.random()
+            map.forEach {
+                if (it.value===current?.link?.first)
+                    array[it.key]--
+                when(kClass.simpleName) {
+                   "UndirWeightGraph", "UndirectedGraph" -> if (it.value===current?.link?.second) array[it.key]--
+                }
+            }
+            graph.deleteEdge(current?.link?.first ?: continue, current.link.second)
+        }
+        for (i in 0..<array.size)
+            assertEquals(array[i], graph.edges[map[i]]?.size ?: 0)
     }
 
 }
