12a6377f2705d026509d48ac46fbff5d33077b02
Darya Nechaeva <necaevadara435@gmail.com>
2025-04-20 21:21:22 +0300

    add basic implementation of the algorithm

--- app/src/main/kotlin/algo/cycles/Cycles.kt
+++ app/src/main/kotlin/algo/cycles/Cycles.kt
@@ -1,4 +1,47 @@
 package algo.cycles
 
-interface Cycles {
+import model.graphs.AbstractGraph
+import model.Vertex
+
+object Cycles {
+    private enum class Color { WHITE, GRAY, BLACK }
+
+    private fun <K, V> findCycles(graph: AbstractGraph<K, V>, start: Vertex<K, V>): Set<List<Vertex<K, V>>> {
+        val cycles = mutableSetOf<List<Vertex<K, V>>>()
+        val currentPath = mutableListOf<Vertex<K, V>>()
+        val color = mutableMapOf<Vertex<K, V>, Color>().apply {
+            graph.vertices.forEach { this[it] = Color.WHITE }
+        }
+
+        val vertexToIndex = mutableMapOf<Vertex<K, V>, Int>()
+
+        fun dfs(current: Vertex<K, V>) {
+            val currentColor = color[current] ?: return
+
+            when (currentColor) {
+                Color.GRAY -> {
+                    val cycleStart = vertexToIndex[current] ?: return
+                    val cycle = currentPath.subList(cycleStart, currentPath.size)
+                    cycles.add(cycle.toList())
+                    return
+                }
+                Color.BLACK -> return
+                Color.WHITE -> {
+                    color[current] = Color.GRAY
+                    vertexToIndex[current] = currentPath.size
+                    currentPath.add(current)
+                    graph.edges[current]?.forEach { edge ->
+                        dfs(edge.link.second)
+                    }
+
+                    color[current] = Color.BLACK
+                    currentPath.removeLast()
+                    vertexToIndex.remove(current)
+                }
+            }
+        }
+
+        dfs(start)
+        return cycles
+    }
 }
