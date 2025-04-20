d878560535a53f960c1fcf981f5dc9411a2388fb
Mikhail <mihailsvirukov@mail.ru>
2025-04-20 10:53:27 +0300

    complete test coverage with reverse of graph check and force tests modify common interface for algorithm family of strongly connected nodes search minor refactor in path tests

--- app/src/main/kotlin/algo/strconnect/StrongConnect.kt
+++ app/src/main/kotlin/algo/strconnect/StrongConnect.kt
@@ -2,7 +2,6 @@ package algo.strconnect
 
 import model.Vertex
 import model.graphs.AbstractGraph
-import java.util.Vector
 
 interface StrongConnect {
     fun <K, V > apply(graph: AbstractGraph<K, V>): ArrayDeque<ArrayDeque<Vertex<K, V>>>
