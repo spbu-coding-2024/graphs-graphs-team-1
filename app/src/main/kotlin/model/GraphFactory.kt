89e06b32fd58cabc5382da609397cbf37de5c9f9
Mikhail <mihailsvirukov@mail.ru>
2025-04-24 21:22:13 +0300

    surround neo4j database methods invocation with try/catch

--- app/src/main/kotlin/model/GraphFactory.kt
+++ app/src/main/kotlin/model/GraphFactory.kt
@@ -1,10 +1,64 @@
 package model
 
+import model.graphs.AbstractGraph
+import model.graphs.*
+import org.neo4j.driver.AuthTokens
+import org.neo4j.driver.GraphDatabase
+import org.neo4j.driver.exceptions.ClientException
+import org.neo4j.driver.exceptions.DatabaseException
+
 class GraphFactory {
-    fun <T> fromJSON(constructor: ()->T): T {
-        var graph =constructor.invoke()
-        return graph
+    companion object {
+        fun <K, V> fromNeo4j(
+            constructor: () -> Graph<K, V>,
+            uri: String,
+            user: String,
+            password: String
+        ): Graph<K, V> {
+            var graph = constructor.invoke()
+            try {
+                var driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
+                var session = driver.session()
+                session.executeRead { transaction ->
+                    val amount = transaction.run(
+                        "MATCH (n) RETURN max(ID(n))"
+                    ).list()[0].get("max(ID(n))")
+                    val vertices = Array<Vertex<K, V>?>(amount.asInt() + 1) { null }
+                    val result = transaction.run(
+                        "MATCH (x: Vertex)-[t]->(y: Vertex) RETURN ID(x) AS fid, x.key AS fk, x.value AS fv, " +
+                                "ID(y) as sid, y.key AS sk, y.value AS sv, t.weight AS weight"
+                    )
+                    for (record in result) {
+                        if (vertices[record["fid"].asInt()] == null)
+                            vertices[record["fid"].asInt()] = Vertex(record["fk"] as K, record["fv"] as V)
+                        if (vertices[record["sid"].asInt()] == null)
+                            vertices[record["sid"].asInt()] = Vertex(record["sk"] as K, record["sv"] as V)
+                        graph.addEdge(
+                            vertices[record["fid"].asInt()] ?: throw IllegalStateException(),
+                            vertices[record["sid"].asInt()] ?: throw IllegalStateException(),
+                            record["weight"].asInt()
+                        )
+                    }
+                }
+                session.close()
+                driver.close()
+            } catch (e: ClientException) {
+                println("Wrong password/login or uri!")
+            } catch (e: DatabaseException) {
+                println("Something wrong with database you are trying to reach!")
+            } catch (e: Exception) {
+                println("Something went wrong!")
+            } finally {
+                return graph
+            }
+        }
+
+        fun <K, V> fromSQLite(constructor: () -> AbstractGraph<K, V>): AbstractGraph<K, V> {
+            TODO()
+        }
+
+        fun <K, V> fromJSON(constructor: () -> AbstractGraph<K, V>): AbstractGraph<K, V> {
+            TODO()
+        }
     }
-    fun <T>  fromSQLite(constructor: ()->T): T? {TODO()}
-    fun <T> fromNeo4j(constructor: ()->T): T? {TODO()}
 }
---
+++ app/src/main/kotlin/model/InternalFormatFactory.kt
@@ -0,0 +1,62 @@
+package model
+
+import model.graphs.AbstractGraph
+import model.graphs.Graph
+import org.neo4j.driver.AuthTokens
+import org.neo4j.driver.GraphDatabase
+import org.neo4j.driver.exceptions.ClientException
+import org.neo4j.driver.exceptions.DatabaseException
+
+
+class InternalFormatFactory {
+    companion object {
+        fun <K, V> toNeo4j(graph: Graph<K, V>, uri: String, user: String, password: String) {
+            try {
+                var driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
+                var session = driver.session()
+                for (vertex in graph.vertices) {
+                    session.executeWrite { transaction ->
+                        var result = transaction.run(
+                            "CREATE (vertex: Vertex {key: \$key, " +
+                                    "value: \$value, " +
+                                    "hash: \$hash})",
+                            mapOf(
+                                "key" to vertex.key,
+                                "value" to vertex.value,
+                                "hash" to vertex.hashCode(),
+                            )
+                        )
+                    }
+                }
+                for (vertex in graph.edges) {
+                    for (edge in vertex.value) {
+                        session.executeWrite { transaction ->
+                            var result = transaction.run(
+                                "MATCH (from:Vertex {key: \$fromKey, value: \$fromValue, hash: \$fromHash}) " +
+                                        "MATCH (to:Vertex {key: \$toKey, value: \$toValue, hash: \$toHash}) " +
+                                        "CREATE (from)-[:CONNECTED {weight: \$weight}]->(to)",
+                                mapOf(
+                                    "fromKey" to edge.link.first.key,
+                                    "fromValue" to edge.link.first.value,
+                                    "fromHash" to edge.link.first.hashCode(),
+                                    "toKey" to edge.link.second.key,
+                                    "toValue" to edge.link.second.value,
+                                    "toHash" to edge.link.second.hashCode(),
+                                    "weight" to edge.weight,
+                                )
+                            )
+                        }
+                    }
+                }
+                session.close()
+                driver.close()
+            } catch (e: ClientException) {
+                println("Wrong password/login or uri!")
+            } catch (e: DatabaseException) {
+                println("Something wrong with database you are trying to reach!")
+            } catch (e: Exception) {
+                println("Something went wrong!")
+            }
+        }
+    }
+}
